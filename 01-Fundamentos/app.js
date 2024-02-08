9e658b8ad35b9e8ede6f6d8b291424535097af36
PeterArce96 <parce0496@gmail.com>
2024-02-08 17:34:29 -0500

    javascript - Introduccion a variables

--- 01-Fundamentos/app.js
+++ 01-Fundamentos/app.js
@@ -1,1 +1,27 @@
-console.log('Hola Mundo');
+// console.log('Hola Mundo');
+
+// Creando una variable llamada "a" al cuál se le asigan un valor de 10;
+// let a = 10;
+// var b = 10;
+// const c = 10;
+
+// Podemos cambiar sus valores
+// a = 20;
+// b = 30;
+// c = 20; No se puede modificar el valor de una constante
+
+// Creamos Variables
+// let a = 10; 
+// let b = 10; 
+// let c = 10; 
+// let d = 10; 
+// let x = a + b; 
+
+// Otra forma de crear varia variables, con comas
+let a = 10, 
+    b = 20, 
+    c = 10, 
+    d = 10, 
+    x = a + b;
+
+console.log(a + b)
