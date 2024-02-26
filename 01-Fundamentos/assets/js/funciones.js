465e70cea2c66a82a185b70f3acdc660f6ebeb9c
PeterArce96 <parce0496@gmail.com>
2024-02-26 18:04:40 -0500

    javascript - retorno de las funciones

--- 01-Fundamentos/assets/js/funciones.js
+++ 01-Fundamentos/assets/js/funciones.js
@@ -3,8 +3,13 @@
 // Todas las funciones tradicionales (que tengan la palabra "function" a la hora de declararla, tienen un objeto implicito llamado "arguments")
 // nombre es un argumento
 function saludar(nombre) {
-    console.log(arguments)
-    console.log('Hola ' + nombre);
+    // console.log(arguments)
+    // console.log('Hola ' + nombre);
+    return [1,2,3,4,5];
+
+    // Las funciones no ejecutan nada después del RETURN,
+    // nunca se va a ejecutar
+    console.log('Soy un códifo que está después del return')
 }
 
 // FUNCIÓN ANÓNIMA
@@ -23,7 +28,36 @@ const saludarFlecha2 = ( nombre ) => {
     console.log('Hola '+ nombre)
 }
 
-saludar('Peter', 40, true, 'Costa Rica');
+const retornoDeSaludar = saludar('Peter', 40, true, 'Costa Rica'); //1
+console.log(retornoDeSaludar)
+
 // saludar2('Peter');
-saludarFlecha();
-saludarFlecha2('Joseph')
+
+// saludarFlecha();
+// saludarFlecha2('Joseph')
+
+function sumar(a, b) {
+    return a+b;
+}
+// Flecha
+// const sumar2 = (a,b) => {
+//     return a+b;
+// }
+
+// función flecha resumida
+// cuando la única línea dentro de la función es el return
+const sumar2 = (a,b) => a+b;
+
+console.log(sumar(1,2))
+console.log(sumar2(1,2))
+
+// Función que retorna un número aleatorio
+function getAleatorio() {
+    return Math.random()*100;
+}
+
+// Función Flecha
+const getAleatorio2 = () => Math.random()*100;
+
+console.log(getAleatorio());
+console.log(getAleatorio2());
