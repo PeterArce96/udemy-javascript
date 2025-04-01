00c10fe6af8d60544a9bb6166ac1e1a716713063
PeterArce96 <parce0496@gmail.com>
2025-04-01 17:33:18 -0500

    javascript - Sets y Gets

--- 01-Fundamentos/assets/js/classes/class.js
+++ 01-Fundamentos/assets/js/classes/class.js
@@ -7,14 +7,26 @@ class Persona {
     nombre = '';
     codigo = '';
     frase = '';
+    comida = '';
 
     constructor( nombre = 'Sin nombre', codigo = 'Sin código', frase = 'Sin frase'){
         // contructor --> un método que se va a ejecutar en el momento que se crea una nueva instancia de la clase Persona
         this.nombre = nombre;
         this.codigo = codigo;
         this.frase = frase;
-
     }
+
+    // Sets y Gets
+    // los sets no pueden tener el mismo nombre de la propiedad
+    // Set --> para establecer un valor
+    // Get --> para recuperar un valor
+    set setComidaFavorita(comida) {
+        this.comida = comida.toUpperCase();
+    }
+    get getComidaFavorita(){
+        return `La comida favorita de ${this.nombre} es ${this.comida}`
+    }
+
 
     // Métodos en las clases
     quienSoy(){
@@ -29,7 +41,13 @@ class Persona {
 
 const spiderman = new Persona('Peter Parker', 'Spider', 'Soy tu amigable vecino Spiderman');
 const ironman = new Persona('Tony Stark', 'Ironman', 'Yo soy Ironman');
-// console.log(spiderman);
-// console.log(ironman);
-ironman.quienSoy();
+
+console.log(ironman);
+
+// ironman.quienSoy();
 spiderman.miFrase();
+
+spiderman.setComidaFavorita = 'El pie de cereza de la tía May';
+
+console.log(spiderman.getComidaFavorita)
+console.log(spiderman);
